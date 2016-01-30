ae379fda5f82df518b7b980e9340722184481150
Emiliano Dalla Verde Marcozzi <edvm@fedoraproject.org>
2016-01-30 06:32:45 -0300

    removing deprecated static files

--- bombear/personal/static/cuadrilla/js/bombero-filter-modals.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-  $('.a-href-bombero-details').on(
-      'click', function () {
-        var bomberoDetailsURL = $(this)[0].dataset.url
-        $( ".modal-bombero-details .modal-body" ).load( bomberoDetailsURL );
-        $( ".modal-bombero-details" ).modal();
-      });
-});
